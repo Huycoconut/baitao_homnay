17f7033553486ee75335138061cd53818d033c27
Huycoconut <vuhuy1262@gmail.com>
2023-10-19 16:56:57 +0700

    them file item, listitem, post_item, screen,

---
+++ lib/drawer.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class menu extends StatelessWidget {
+  const menu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Scaffold(
+        drawer: Drawer(
+          child: ListTile (),
+        ),
+      ),
+    );
+  }
+}
